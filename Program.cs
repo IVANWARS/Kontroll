a26bb6d475f7b38f25b95e9882dc8195a80ac7e6
IVANWARS230 <IVANWARS230@yandex.ru>
2024-05-06 22:23:21 +0300

    Добавление примеров

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string [] array = new string [] {};
+String fromUser = ReadImput ();
+switch (fromUser)
+{
+    case "1":
+    array = new string[] {};
+    break;
+
+    case "2":
+    array = new string [] {};
+    break;
+
+    case "3":
+    array = new string [] {};
+    break;
+}
+
