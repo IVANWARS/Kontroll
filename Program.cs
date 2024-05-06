8f441ed14eb29b47541904cbe34378538ba36768
IVANWARS230 <IVANWARS230@yandex.ru>
2024-05-06 23:37:37 +0300

    Вывод команд

--- Program.cs
+++ Program.cs
@@ -13,5 +13,34 @@ switch (fromUser)
     case "3":
     array = new string [] {};
     break;
+
+
+default:
+Console.WriteLine ( $ { fromUser } );
 }
+
+int lennewarray = 0 ;
+for (int i = 0; i <= array.Length -1; i++)
+{
+    if (array.Length <= 3) lennewarray++;
+
+}
+ 
+ string[] newarray = new string [lennewarray];
+ int idx = 0;
+
+ for (int i = 0; i <= array.Length -1; i++)
+ {
+    if (array.Length <= 3)
+    {
+        newarray[idx] = array[i];
+        idx++;
+    }
+ }
+     
+ PintArray( array);
+ Console.Write("");
+ PrintArray(newarray);
+
+ 
 
